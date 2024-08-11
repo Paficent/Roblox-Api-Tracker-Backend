564756e4c85286460b64a2c57c134ddb08a1e299
Paficent <78674390+Paficent@users.noreply.github.com>
2024-08-10 20:32:17 -0400

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-function ollama_query() {
-  curl -X POST http://localhost:11434/api/generate -d "{\"model\": \"mistral\", \"prompt\": \"$1\"}"
-}
+git_diff=$(git diff -summary -b)
+changed_files=$(git diff --name-only | awk -F '.' '{print $1}')
+prompt="This git diff describes changes made to Roblox's api using OpenAPI json documentation format. For example, if a route 'getAccountAge' is added, an example response would be 'Added route getAccountAge to JSON_FILE_NAME_PLACEHOLDER.', of course replacing with the actual json file name. Summarize this diff in 7 words or less per file: $git_diff"
 
-git_diff=$(git diff)
-prompt="Summarize this git diff into a useful, 10 words commit message in english: $git_diff"
+response=$(curl -X POST http://localhost:11434/api/generate -d "{\"model\": \"mistral\", \"prompt\": \"$prompt\"}")
+python -c "import json, sys; print(json.loads(sys.stdin.read())['response'])" <<< "$response"
 
-response=$(ollama_query "$prompt")
-
-commit_message=$(echo "$response" | jq '.response' | tr -d '"')
-commit_message=$(echo "$commit_message" | tr -d '[:space:]')
-
-# Perform the git commit
-git commit -m "$commit_message"
+git commit -m "Ai Generated Summary: $commit_message\nFiles Changed: $changsd_files"
