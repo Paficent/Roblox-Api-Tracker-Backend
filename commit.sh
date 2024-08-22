e3f8850945d680b89c93d9c6b30abae2fc9a6518
Paficent <78674390+Paficent@users.noreply.github.com>
2024-08-21 21:24:00 -0400

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@
 
 git_diff=$(git diff --summary -b)
 changed_files=$(git diff --name-only | awk -F '.' '{print $1}')
-prompt="This git diff describes changes made to Roblox's api using OpenAPI json documentation format. For example, if a route 'getAccountAge' is added, an example response would be 'Added route getAccountAge to JSON_FILE_NAME_PLACEHOLDER.', of course replacing with the actual json file name. Summarize this diff in 7 words or less per file: $git_diff"
+prompt="This git diff describes changes made to Roblox's api using OpenAPI json documentation format. For example, if a route 'getAccountAge' is added, an example response would be 'Added route getAccountAge to PLACEHOLDER_API_NAME/PLACEHOLDER_VERSION.json.', of course replacing PLACEHOLDER_API_NAME and PLACEHOLDER_VERSION with the actual api name and version (version is usually v1.json). Summarize this diff in 7 words or less per file: $git_diff"
 
 response=$(curl -X POST http://localhost:11434/api/generate -d "{\"model\": \"mistral\", \"prompt\": \"$prompt\"}")
 python -c "import json, sys; print(json.loads(sys.stdin.read())['response'])" <<< "$response"
