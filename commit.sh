97160ec549d2d48325e80fb0e86ee044bba1c98b
Paficent <78674390+Paficent@users.noreply.github.com>
2024-08-21 22:12:34 -0400

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -5,7 +5,7 @@ git_diff=$(git diff --summary -b)
 changed_files=$(git diff --name-only | awk -F '.' '{print $1}')
 
 input="Summarize the following git diff in a commit message: $git_diff"
-commit_message=$(ollama run tavernari/git-commit-message --input "$input")
+commit_message=$(ollama run tavernari/git-commit-message "$input")
 
 # Check if the model returned a message
 if [ -z "$commit_message" ]; then
